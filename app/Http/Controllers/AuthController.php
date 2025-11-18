92d82ecd51f7776241f717689175cd473bead885
ossameliaz-crypto <ossameliaz@student.ub.ac.id>
2025-11-18 15:49:18 +0700

    benerin login post man

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
