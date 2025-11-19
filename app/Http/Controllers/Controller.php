4a7e76b86a675fce151456c663756f7b3765067f
shelfina <shelfinaanindita908@gmail.com>
2025-11-19 20:13:18 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,10 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
-<<<<<<< HEAD
 {  
-=======
-{
     
->>>>>>> 19d31a6179b24629ab3219a574ffb1e1c6ce9f78
 }
