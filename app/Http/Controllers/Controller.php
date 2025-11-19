777621550de2ae8c2196f841cea81ebd9a075134
shelfina <shelfinaanindita908@gmail.com>
2025-11-19 20:05:02 +0700

    feat: Benerin aja

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-// Import BaseController dari framework
 use Illuminate\Routing\Controller as BaseController;
 
-// Pastikan class Controller-mu extends BaseController
 class Controller extends BaseController
-{
-    // (Tidak perlu 'use' aneh-aneh di sini)
-    // (Kosongan saja tidak apa-apa)
+{  
 }
